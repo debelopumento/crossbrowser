a403226ffc856e87f8c73b406796a05d72cdc9a6
debelopumento <imdiye@gmail.com>
2017-11-20 09:42:55 -0800

    added button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  clickedButton = () => {
+    console.log('button clicked')
+  }
   render() {
     return (
       <div className="App">
@@ -13,9 +16,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.clickedButton}>button</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
